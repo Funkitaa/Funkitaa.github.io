96ae60435cdf197154e523b99d40741ee40a90c3
Funkitaa <125930475+Funkitaa@users.noreply.github.com>
2023-03-05 21:44:41 -0300

    initial commit

---
+++ proyecto.js
@@ -0,0 +1,11 @@
+
+document.getElementById('mail').addEventListener('click', function() {
+    console.log("Email CV");
+    document.getElementById("email").innerHTML = "anais.pierre@example.com"
+})
+
+document.getElementById('tel').addEventListener('click', function() {
+    console.log("Telefono CV");
+    document.getElementById("telinfo").innerHTML = "06-02-74-46-29"
+})
+
